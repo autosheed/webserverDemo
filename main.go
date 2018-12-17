82af675de9b9508ddd439379cf1916621ab7a258
auto_sheed <442364680@qq.com>
2018-12-17 20:57:13 +0800

    v6

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func run() {
 	http.HandleFunc("/",blockchainGetHandle)
-	http.ListenAndServe("localhost:79",nil)
+	http.ListenAndServe(":8000",nil)
 }
 
 func blockchainGetHandle(w http.ResponseWriter,r *http.Request) {
-	io.WriteString(w,"<h1>hello , i am webserver v5</h1>")
+	io.WriteString(w,"<h1>hello , i am webserver v6</h1>")
 }
 
 func main() {
